85ed90e164f64ae8a0a051024cb4fb61c355694f
Martin Halamíček <martin@halamicek.cz>
2017-04-05 14:01:27 +0200

    push to ECR

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# taken from https://gist.github.com/BretFisher/14cd228f0d7e40dae085
-# install aws cli w/o sudo
-pip install --user awscli
-# put aws in the path
-export PATH=$PATH:$HOME/.local/bin
-# needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
-eval $(aws ecr get-login --region us-east-1)
-docker tag keboola/docker-demo-app:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-demo-app:$TRAVIS_TAG
-docker tag keboola/docker-demo-app:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-demo-app:latest
-docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-demo-app:$TRAVIS_TAG
-docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-demo-app:latest
+docker pull quay.io/keboola/aws-cli
+eval $(docker run --rm -i -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY quay.io/keboola/aws-cli ecr get-login --region us-east-1)
+docker tag keboola/docker-demo-app:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/sapi-merged-export:$TRAVIS_TAG
+docker tag keboola/docker-demo-app:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/sapi-merged-export:latest
+docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/sapi-merged-export:$TRAVIS_TAG
+docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/sapi-merged-export:latest
