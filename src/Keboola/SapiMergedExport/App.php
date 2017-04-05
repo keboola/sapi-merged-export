1f4dbee250ee014a3d9da6d436067b656897c360
Martin Halamíček <martin@halamicek.cz>
2017-04-05 09:07:36 +0200

    move files to out init

--- src/Keboola/SapiMergedExport/App.php
+++ src/Keboola/SapiMergedExport/App.php
@@ -8,11 +8,30 @@
 
 namespace Keboola\SapiMergedExport;
 
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
 class App
 {
+    private $finder;
+    private $fileSystem;
+
+    public function __construct()
+    {
+        $this->finder = new Finder();
+        $this->fileSystem = new Filesystem();
+    }
 
     public function run($inputTablesFolderPath, $outputFilesFolderPath)
     {
-        // todo
+        array_map(function (SplFileInfo $file) use ($outputFilesFolderPath) {
+            $this->processFile($file, $outputFilesFolderPath);
+        }, iterator_to_array($this->finder->files()->in($inputTablesFolderPath)));
+    }
+
+    private function processFile(SplFileInfo $file, $outputFilesFolderPath)
+    {
+        $this->fileSystem->copy($file->getRealPath(), $outputFilesFolderPath . '/' . $file->getBasename());
     }
 }
--- tests/Keboola/SapiMergedExport/AppTest.php
+++ tests/Keboola/SapiMergedExport/AppTest.php
@@ -2,13 +2,34 @@
 
 namespace Keboola\DockerDemo;
 
+use Keboola\SapiMergedExport\App;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 class AppTest extends TestCase
 {
 
     public function testProcess()
     {
-        // todo
+        // create data dirs
+        $fs = new Filesystem();
+        $finder = new Finder();
+        $inputTablesDir = sys_get_temp_dir() . '/input';
+        $outputFilesDir = sys_get_temp_dir() . '/output';
+        $fs->mkdir([$inputTablesDir, $outputFilesDir]);
+
+        // create test files
+        $fs->dumpFile($inputTablesDir . '/in.c-main.test.csv', <<< EOF
+id,text,some_other_column
+1,"Short text","Whatever"
+2,"Long text Long text Long text","Something else"
+EOF
+        );
+
+        $app = new App();
+        $app->run($inputTablesDir, $outputFilesDir);
+
+        $this->assertCount(1, $finder->files()->in($outputFilesDir));
     }
 }
